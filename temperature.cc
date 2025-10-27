d06e98fc85615c6a7da08f5b365754f3dd8c54c0
imingo07 <66923843+imingo07@users.noreply.github.com>
2025-10-27 08:51:27 -0400

    Variables for analog conversion
    Updated temperature monitoring program with improved variable definitions for analog conversion.

--- temperature.cc
+++ temperature.cc
@@ -3,13 +3,13 @@
 
 
 // Defining h-bridge pins and encoder pins - more physical so not used in this version
-const int lpwn_pin = D5;      
-const int rpwm_pin = D6;      
-const int rEnable_pin = D7;  
-const int lEnable_pin = D8;  
+const int lpwn_pin = 5;      
+const int rpwm_pin = 6;      
+const int rEnable_pin = 7;  
+const int lEnable_pin = 8;  
 
-const int encoderA = D2; 
-const int encoderB = D3; 
+const int encoderA = 2; 
+const int encoderB = 3; 
 
 // Setting A0 to current sensor pin
 const int temp_sensor = A0;
@@ -23,11 +23,17 @@ const float overheatTemp = 50.0; // Max temperature (TBD)
 float currentTemp = 0.0;
 bool isOverheated = false; 
 
+// variables for analog conversion
+const float bitRange = 1023.0;
+const float inputVoltage = 5.0;
+const float offsetVoltage = 0.5;
+const float scale = 100.0;
+
 //Converts analog value to temp in Celsius - not certain if conversion is correct
 float readTemperature() {
   int analogValue = analogRead(temp_sensor);
-  float volt = (analogValue * 5.0) / 1023.0;
-  float currentTemp = (volt - 0.5) * 100.0;
+  float volt = (analogValue * inputVoltage) / bitRange;
+  float currentTemp = (volt - offsetVoltage) * scale;
   return currentTemp;
 }
 
@@ -83,4 +89,5 @@ void loop() {
   }
   
   delay(500); 
+
 }
