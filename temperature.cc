325e982d14ff03936773393ceeb13798878e55e5
imingo07 <66923843+imingo07@users.noreply.github.com>
2025-11-09 14:18:23 -0500

    Adding functions

--- temperature.cc
+++ temperature.cc
@@ -1,27 +1,12 @@
-// Program monitors temperature, starts shutdown process if temperature is too high
-//This version basically is checking for safe temps, more physical actions are commented out.
-
-
-// Defining h-bridge pins and encoder pins - more physical so not used in this version
-const int lpwn_pin = 5;      
-const int rpwm_pin = 6;      
-const int rEnable_pin = 7;  
-const int lEnable_pin = 8;  
-
-const int encoderA = 2; 
-const int encoderB = 3; 
+// Program monitors temperature, signals shutdown process if temperature is too high
 
 // Setting A0 to current sensor pin
 const int temp_sensor = A0;
 
-
-// Define encoder count variable - not used in this version
-volatile long encoderCount = 0;
-
 // Temperature variables and threshold
 const float overheatTemp = 50.0; // Max temperature (TBD)
 float currentTemp = 0.0;
-bool isOverheated = false; 
+bool isOverheated = false;
 
 // variables for analog conversion
 const float bitRange = 1023.0;
@@ -29,7 +14,7 @@ const float inputVoltage = 5.0;
 const float offsetVoltage = 0.5;
 const float scale = 100.0;
 
-//Converts analog value to temp in Celsius - not certain if conversion is correct
+// Function to calculate temperature from analog read
 float readTemperature() {
   int analogValue = analogRead(temp_sensor);
   float volt = (analogValue * inputVoltage) / bitRange;
@@ -37,45 +22,25 @@ float readTemperature() {
   return cTemp;
 }
 
-void updateEncoderCount() {
-  // Encoder code to change count
-  // Not used in this version
-}
-
-//initalizing system
-void setup() {
-  Serial.begin(9600);
-  
-  // Encoder + h-bridge setup - not used in this version
-  pinMode(encoderA, INPUT_PULLUP);
-  pinMode(encoderB, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(encoderA), updateEncoderCount, CHANGE);
-
-  pinMode(lpwn_pin, OUTPUT);
-  pinMode(rpwm_pin, OUTPUT);
-  pinMode(rEnable_pin, OUTPUT);
-  pinMode(lEnable_pin, OUTPUT);
-
-}
-
-// Recurring loop that checks conditions for safe/unsafe temps
-void loop() {
+// Function to report temperature
+void reportTemp() {
   currentTemp = readTemperature();
-// Overheat Check
+
+  // Overheat Check 
   if (currentTemp >= overheatTemp) {
     if (!isOverheated) {
       Serial.println("OVERHEAT");
       isOverheated = true;
     }
-// OK Temp Check
   } else {
+    // OK Temp Check 
     if (isOverheated) {
       Serial.println("OK");
       isOverheated = false;
     }
   }
 
-  // Current status w/ delay
+  // Current status report
   Serial.print("Temp: ");
   Serial.print(currentTemp);
   Serial.print(" °C | Status: ");
@@ -84,6 +49,16 @@ void loop() {
   } else {
     Serial.println("OK");
   }
+}
+
+// initializing system
+void setup() {
+  Serial.begin(9600);
+}
+
+// Loop that calls the report function w/ delay
+void loop() {
+  reportTemp();
 
   delay(500);
 }
