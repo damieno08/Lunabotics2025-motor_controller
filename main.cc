ea970138e69f37b57546b72c1473dc5599ea0fe6
Damien Ortiz <damieno2006@gmail.com>
2025-11-30 11:17:21 -0500

    Setup function for system initialization
    Initialize pins and setup for sensors and servo.

--- main.cc
+++ main.cc
@@ -1,3 +1,33 @@
+#include "functions.h"
+
+// setup all functions and pins
+//
+void setup() {
+  Serial.begin(9600);
+
+  // Current sensor
+  //
+  pinMode(CURRENT_PIN, INPUT);
+
+  // Encoder
+  //
+  pinMode(ejectoseato, INPUT_PULLUP);
+  pinMode(e4tPinA, INPUT_PULLUP);
+  pinMode(e4tPinB, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(e4tPinA), updateEncoderCount, CHANGE);
+
+  // Hall and Photodiode
+  //
+  pinMode(hallEffectPin, INPUT);
+  pinMode(photoPin, INPUT);
+
+  // Servo
+  //
+  myServo.attach(9);
+
+  Serial.println("System Initialized.");
+}
+
 // call all functions
 //
 void loop() {
