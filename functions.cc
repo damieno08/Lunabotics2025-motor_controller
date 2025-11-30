d67913faa0b2e9cc9e1d7b112f43de4af81d0c83
Damien Ortiz <damieno2006@gmail.com>
2025-11-30 11:15:29 -0500

    Remove setup function and initialization code
    Removed the setup function and its initialization code.

--- functions.cc
+++ functions.cc
@@ -141,27 +141,4 @@ void reportTemp() {
   Serial.println(isOverheated ? "OVERHEAT" : "OK");
 }
 
-/* ============================================================
-   FINAL SETUP
-   ============================================================ */
-void setup() {
-  Serial.begin(9600);
 
-  // Current sensor
-  pinMode(CURRENT_PIN, INPUT);
-
-  // Encoder
-  pinMode(ejectoseato, INPUT_PULLUP);
-  pinMode(e4tPinA, INPUT_PULLUP);
-  pinMode(e4tPinB, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(e4tPinA), updateEncoderCount, CHANGE);
-
-  // Hall and Photodiode
-  pinMode(hallEffectPin, INPUT);
-  pinMode(photoPin, INPUT);
-
-  // Servo
-  myServo.attach(9);
-
-  Serial.println("System Initialized.");
-}
